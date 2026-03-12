d247005bc7112e491c7d50d0a7714e54122cb6f0
hanenashi <hanenashi@gmail.com>
2026-03-12 18:53:55 +0900

    Update theme-mobile.js

--- modules/theme-mobile.js
+++ modules/theme-mobile.js
@@ -10,24 +10,6 @@ if (!meta) {
 }
 meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0';
 
-// 2. CSS Resets & Anti-Autosizer Fix
-const mobileFixCSS = `
-    /* Stop Okoun's ancient tables from causing horizontal scrolling */
-    html, body { 
-        max-width: 100vw; 
-        overflow-x: hidden; 
-    }
-    
-    /* Kill Chromium / Kiwi Text Autosizer (Font Boosting Bug) */
-    html, body {
-        -webkit-text-size-adjust: 100% !important;
-        text-size-adjust: 100% !important;
-    }
-    p, .content, .content .yui-base {
-        max-height: 999999px !important; 
-    }
-`;
-
 GM.addStyle(mobileFixCSS);
 
 log('SNIFF', 'Viewport forced to mobile scale & Anti-Autosizer injected.');
