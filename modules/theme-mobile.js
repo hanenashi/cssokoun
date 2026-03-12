02dba563ed3d232362c4c8d4095e62d8f422e9ac
hanenashi <hanenashi@gmail.com>
2026-03-12 18:54:52 +0900

    Update theme-mobile.js

--- modules/theme-mobile.js
+++ modules/theme-mobile.js
@@ -1,7 +1,7 @@
 const log = (level, ...args) => window.cssokoun.log(level, 'theme-mobile-js', ...args);
 log('INFO', 'Injecting Mobile Viewport Meta Tag...');
 
-// 1. Force the mobile viewport
+// 1. Force the mobile viewport so the CSS actually triggers on phones
 let meta = document.querySelector('meta[name="viewport"]');
 if (!meta) {
     meta = document.createElement('meta');
@@ -10,6 +10,4 @@ if (!meta) {
 }
 meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0';
 
-GM.addStyle(mobileFixCSS);
-
-log('SNIFF', 'Viewport forced to mobile scale & Anti-Autosizer injected.');
+log('SNIFF', 'Viewport forced to mobile scale.');
